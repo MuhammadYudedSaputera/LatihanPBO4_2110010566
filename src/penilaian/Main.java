9a43570ae0b74a3aa98be43929c13793bc9d490d
MuhammadYudedSaputera <gabinamang@gmail.com>
2023-06-13 12:14:08 +0800

    membuat program latihan4

--- src/penilaian/Main.java
+++ src/penilaian/Main.java
@@ -1,14 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package penilaian;
 
-/**
- *
- * @author ASUS
- */
 public class Main {
-    
+   public static void main(String[] args) {
+        // Membuat objek Mahasiswa
+        Mahasiswa mahasiswa = new Mahasiswa();
+        mahasiswa.setNama("John Doe");
+        mahasiswa.setNpm("1234567890");
+        
+        // Membuat objek NilaiAkhir
+        NilaiAkhir nilaiAkhir = new NilaiAkhir();
+        nilaiAkhir.setUts(80);
+        nilaiAkhir.setUas(85);
+        nilaiAkhir.setTugas(90);
+        
+        // Menampilkan informasi mahasiswa dan nilai akhirnya
+        System.out.println("Nama Mahasiswa: " + mahasiswa.getNama());
+        System.out.println("NPM Mahasiswa: " + mahasiswa.getNpm());
+        System.out.println("Nilai UTS: " + nilaiAkhir.getUts());
+        System.out.println("Nilai UAS: " + nilaiAkhir.getUas());
+    }
 }
--- src/penilaian/NilaiAkhir.java
+++ src/penilaian/NilaiAkhir.java
@@ -2,29 +2,41 @@
 package penilaian;
 
 public class NilaiAkhir {
-    private int uts, uas, tugas;
-
-    public NilaiAkhir(String Nama, String npm, int uts, int uas, int tugas) {
-        super.nama = nama;
-        super.npm = npm;
+   private double uts;
+    private double uas;
+    private double tugas;
+    
+    public double getUts() {
+        return uts;
+    }
+    
+    public void setUts(double uts) {
         this.uts = uts;
+    }
+    
+    public double getUas() {
+        return uas;
+    }
+    
+    public void setUas(double uas) {
         this.uas = uas;
+    }
+    
+    public double getTugas() {
+        return tugas;
+    }
+    
+    public void setTugas(double tugas) {
         this.tugas = tugas;
     }
     
-    @override
-    public void setNpm(String npm) {
-        super.setNpm (npm)
-    }
-    
-    @override
-    public String getNpm() {
-        return super.getNpm();
-    }
-    
-    
-    public int hitungNilaiAkhir(){
-        return (int) (0.3*uts + 0.3*uas + 0.4*tugas)
+    public double hitungNilaiAkhir() {
+        // Menggunakan bobot 40% UTS, 40% UAS, dan 20% tugas
+        double bobotUts = 0.4;
+        double bobotUas = 0.4;
+        double bobotTugas = 0.2;
         
- }
+        return (uts * bobotUts) + (uas * bobotUas) + (tugas * bobotTugas);
+    }
 }
+
