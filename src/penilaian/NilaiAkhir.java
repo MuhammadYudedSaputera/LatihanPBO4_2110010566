63d9d40e40189ee5924c366fb9ec4ae790250126
MuhammadYudedSaputera <gabinamang@gmail.com>
2023-06-13 09:52:10 +0800

    mahasiswa1

--- src/penilaian/NilaiAkhir.java
+++ src/penilaian/NilaiAkhir.java
@@ -4,13 +4,26 @@ package penilaian;
 public class NilaiAkhir {
     private int uts, uas, tugas;
 
-    public NilaiAkhir(int uts, int uas, int tugas) {
+    public NilaiAkhir(String Nama, String npm, int uts, int uas, int tugas) {
+        super.nama = nama;
+        super.npm = npm;
         this.uts = uts;
         this.uas = uas;
         this.tugas = tugas;
     }
     
-    public NilaiAkhir(){
+    @override
+    public void setNpm(String npm) {
+        super.setNpm (npm)
+    }
+    
+    @override
+    public String getNpm() {
+        return super.getNpm();
+    }
+    
+    
+    public int hitungNilaiAkhir(){
         return (int) (0.3*uts + 0.3*uas + 0.4*tugas)
         
  }
